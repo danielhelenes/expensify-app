08dbf35a132e6276ea926fff4c02f407e5ba0e31
Daniel Ferro <danielferro@Daniels-MacBook-Pro.local>
2018-04-05 22:29:48 +0100

    router components text updated

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
-import Header from '../components/Header.js';
-import HelpPage from '../components/HelpPage.js';
-import AddExpensePage from '../components/AddExpensePage.js';
-import EditExpensePage from '../components/EditExpensePage.js';
-import ExpenseDashboardPage from '../components/ExpenseDashboardPage.js';
-import NotFoundPage from '../components/NotFoundPage.js';
+import Header from '../components/Header';
+import HelpPage from '../components/HelpPage';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import NotFoundPage from '../components/NotFoundPage';
 
 const AppRouter = () => ( //the API from BrowserRouter expects us to only have the length of 0 or 1 inside. that's why we put things in a div.
     <BrowserRouter>
